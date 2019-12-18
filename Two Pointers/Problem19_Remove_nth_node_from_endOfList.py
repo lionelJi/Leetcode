3c39134b565eddfa0760c2e061ff6f0484ef0026
Jiaqing <lionelji0225@gmail.com>
2019-12-18 16:50:43 +0800

    AC

---
+++ Two Pointers/Problem19_Remove_nth_node_from_endOfList.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution:
+    def removeNthFromEnd(self, head, n):
+        if not head or not n:
+            return None
+
+        dummy = ListNode(-1)
+        dummy.next = head
+
+        fast, slow = dummy, dummy
+        for i in range(n):
+            fast = fast.next
+
+        while fast.next:
+            fast = fast.next
+            slow = slow.next
+
+        slow.next = slow.next.next
+
+        return dummy.next
